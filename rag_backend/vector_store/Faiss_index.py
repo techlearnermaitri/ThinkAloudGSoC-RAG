a991f81c5f5ca87f85306c6abeab8d9048bf7f70
Maitri <176602851+techlearnermaitri@users.noreply.github.com>
2026-01-12 22:11:15 +0530

    Add FAISS index building function

---
+++ rag_backend/vector_store/Faiss_index.py
@@ -0,0 +1,8 @@
+import faiss
+import numpy as np
+
+def build_faiss_index(embeddings):
+    dim = embeddings.shape[1]
+    index = faiss.IndexFlatL2(dim)
+    index.add(embeddings)
+    return index
